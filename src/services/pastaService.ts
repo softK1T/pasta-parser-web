959429b9cd11363c81dd9ea63265d99addaf7fd7
softK1T <nazar12522@gmail.com>
2024-09-01 19:40:40 +0200

    api_url change

--- src/services/pastaService.ts
+++ src/services/pastaService.ts
@@ -1,21 +1,21 @@
 import axios from "axios";
-import { Pasta } from "../types/Pasta";
+import {Pasta} from "../types/Pasta";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://192.168.1.102:8000";
 
 export const fetchPastas = async (
-  sort_by: string = "timestamp",
-  order: number = -1,
-  limit: number = 20, // Default limit
-  offset: number = 0 // Default offset
+    sort_by: string = "timestamp",
+    order: number = -1,
+    limit: number = 20, // Default limit
+    offset: number = 0 // Default offset
 ): Promise<Pasta[]> => {
-  const response = await axios.get(`${API_URL}/pastas`, {
-    params: { sort_by, order, limit, offset },
-  });
-  return response.data;
+    const response = await axios.get(`${API_URL}/pastas`, {
+        params: {sort_by, order, limit, offset},
+    });
+    return response.data;
 };
 
 export const fetchPastaById = async (id: number): Promise<Pasta> => {
-  const response = await axios.get(`${API_URL}/pasta/${id}`);
-  return response.data;
+    const response = await axios.get(`${API_URL}/pasta/${id}`);
+    return response.data;
 };
